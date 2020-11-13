6fb5308c3772fe47a69e02baf37f896b661b1cca
Alan Herr <39202916+mutatorUW@users.noreply.github.com>
2020-11-12 16:25:12 -0800

    Update Fig3hij.py

--- Fig3hij.py
+++ Fig3hij.py
@@ -1,4 +1,4 @@
-# For Fig.3(h-j), Dowsett et al.
+# For Fig.3(g-j), Dowsett et al.
 import glob
 import pandas as pd
 import numpy as np
@@ -46,10 +46,10 @@ def chr_mut_p(chr_mu_dict,c):
     return {k:(poisson.rvs(mu=v, size=c)) for (k,v) in chr_mu_dict.items()}
 
 # as above, except rate is doubled to represent actual rate of mispair formation.
-def chr_mut_bp(chr_mu_dict,c):
+def chr_mut_pb(chr_mu_dict,c):
     return {k:(poisson.rvs(mu=(v*2), size=c)) for (k,v) in chr_mu_dict.items()}
 
-# multiplies value by randomly selected 0 or 1 to mimic Mendelian segregation.
+# multiplies value by randomly selected 0 or 1 to mimic mitotic segregation.
 def binomialize(x):
     return x * (random.randint(0,1))
 
@@ -64,15 +64,30 @@ def mk_df_p(chr_mu_dict,c):
     df_p["sum"]=df_p.sum(axis=1)
     return df_p
 
-# simulates c iterations of fixed mutations counts arising by a binomial-Poisson process.
-def mk_df_bp(chr_mu_dict,c):
-    data_poisson2_p = chr_mut_bp(chr_mu_dict,c)
+# simulates c iterations of fixed mutations counts arising by a Poisson-binomial process.
+def mk_df_pb(chr_mu_dict,c):
+    data_poisson2_p = chr_mut_pb(chr_mu_dict,c)
     dfas_p = pd.DataFrame.from_dict(data_poisson2_p)
     df2_p = dfas_p.applymap(binomialize) #Mimics segregation of each chromosome.
     df2_p["sum"]=df2_p.sum(axis=1)
     return df2_p
+# Extrapolating yeast mutation rates to human ultramutator cells.
+def ye2hu_mr(yeast_1n_mu):
+    hu_mu = (tot_bp/11e7)*yeast_1n_mu
+    return hu_mu
+
+# Input parameters from glm.nb in R for the combined distribution of Dm and Mm counts.
+munb = 4.927 # rate parameter for negative binomial (munb)
+theta = 60.42 # shape parameter
+
+# Conversions
+muc = math.exp(munb) #mu corrected (muc) is munb exponentiated.
+gv = (muc)**2/theta #variance of the Gamma distribution of lambda
+gscale = gv/muc #scale paramater of the Gamma distribution of lambda.
+hap_mu = muc/4 #rate of fixed mutations per haploid genome.
 
 n = 10000 # desired number of iterations to perform.
+yeast_1n_mu = 34.5 #mutations/divisions/genome
 
 #Gathers the number of nucleotides on each chr in human genome (a and b for 2n cell)
 chr_list_a = []
@@ -88,46 +103,83 @@ for chrfile in glob.glob(r"G:\My Drive\Volatility File Share\HumanGenome\*.fa"):
     chr_lenlist.append(count)
 tot_bp = sum(chr_lenlist)
 
-# Extrapolating yeast mutation rates to human ultramutator cells.
-yeast_1n_mu = 34.5 #mutations/divisions/genome
-print("With a yeast haploid mutation rate of", yeast_1n_mu)
-hu_mu = (tot_bp/11e7)*yeast_1n_mu
-print("the comparable human haploid mutation rate is", hu_mu)
-
-#Generates initial Poisson and binomial-Poisson distributions for comparisons.
-mu=hu_mu
+#Generates initial Poisson and Poisson-binomial distributions for comparisons.
+mu=ye2hu_mr(34.5)
 counter = 0
 Dlist=[]
 D2list=[]
 chr_mu_dict = mk_chr_mu_dict(mu,chr_lenlist,tot_bp,chr_list_a,chr_list_b)
 df_p = mk_df_p(chr_mu_dict,n)
-df_bp = mk_df_bp(chr_mu_dict,n)
+df_pb = mk_df_pb(chr_mu_dict,n)
 print("Index of Dispersion for Poisson, n=",n,",",index_dis(df_p))
-print("Index of Dispersion for binomial-Poisson, n=",n,",",index_dis(df_bp))
+print("Index of Dispersion for Poisson-binomial, n=",n,",",index_dis(df_pb))
 
-# Simulates the change in mutation burden in individual tumor cell lineages over 10 divisions.
+#Generates gamma-Poisson-binomial distribution.
+"""The following returns a gamma distribution of rates of mismatch formation
+and then divides each value by 4, since we're looking at the number of fixed mutations
+per haploid genome. This distribution will be our input for n divisions,
+each using a different lambda. Note that chr_mut_pb takes these values and doubles them
+to account for the actual rate of mispair formation.  This then becomes halved when we
+binomialize it to account for segregation."""
+data_gamma = (gamma.rvs(a=theta, scale=gscale, size=n))/4
+dfas_nb = pd.DataFrame(columns=chr_list_a+chr_list_b)
+for g in data_gamma:
+    hu_g = ye2hu_mr(g)
+    chr_mu_dict = mk_chr_mu_dict(hu_g,chr_lenlist,tot_bp,chr_list_a,chr_list_b)
+    data_poisson2 = chr_mut_pb(chr_mu_dict, 1)
+    dfa2 = pd.DataFrame.from_dict(data_poisson2)
+    dfas_nb = dfas_nb.append(dfa2, ignore_index=True)
+df2_nb = dfas_nb.applymap(binomialize)
+df2_nb["sum"] = df2_nb.sum(axis=1)
+print("Index of Dispersion for gamma-Poisson-binomial, n=",n,",",index_dis(df2_nb))
+
+#Simulates how dispersion due to the Poisson-binomial process varies as a function of mutation rate.
+hu_mu_rng=list(range(1,951))
+Drlist=[]
+D2rlist=[]
+for i in hu_mu_rng:
+    mu=i
+    chr_mu_dict = mk_chr_mu_dict(mu,chr_lenlist,tot_bp,chr_list_a,chr_list_b)
+    df_p = mk_df_p(chr_mu_dict,n)
+    df_bp = mk_df_bp(chr_mu_dict,n)
+    Drlist.append(index_dis(df_p))
+    D2rlist.append(index_dis(df_bp))
+Df_IDrng_p=pd.DataFrame()
+Df_IDrng_pb=pd.DataFrame()
+Df_IDrng_p["rate"]=hu_mu_rng
+Df_IDrng_p["ID"]=Drlist
+Df_IDrng_pb["rate"]=hu_mu_rng
+Df_IDrng_pb["ID"]=D2rlist
+Df_IDrng_pb.to_csv(f"G:\My Drive\Volatility File Share\Figures\IDrng_p.csv",index=False)
+Df_IDrng_pb.to_csv(f"G:\My Drive\Volatility File Share\Figures\IDrng_pb.csv",index=False)
+print("Linear regression of relationship between MR and ID with pois model:")
+linregparams(Df_IDrng_p)
+print("Linear regression of relationship between MR and ID with poisbin model:")
+linregparams(Df_IDrng_pb)
+
+# Simulates the change in mutation burden in individual tumor cell lineages over 30 divisions.
 n = 1000
 counter = 0
 plist=[] #
-bplist=[]
+pblist=[]
 dfRT=pd.DataFrame() #To keep track of cumulative mutation burden during a Poisson process.
 dfRT["Div"]=list(range(1,31,1))
-df2RT=pd.DataFrame() #To keep track of cumulative mutation burden during a binomial-Poisson process.
+df2RT=pd.DataFrame() #To keep track of cumulative mutation burden during a Poisson-binomial process.
 df2RT["Div"]=list(range(1,31,1))
 while counter < n:
     df_p_line = mk_df_p(chr_mu_dict,30)
     dfRT[counter+1]=df_p_line["sum"].cumsum()
     mutbur_p=df_p_line['sum'].sum()
     plist.append(mutbur_p)
-    df_bp_line = mk_df_bp(chr_mu_dict,30)
-    df2RT[counter+1]=df_bp_line["sum"].cumsum()
-    mutbur_bp=df_bp_line['sum'].sum()
-    bplist.append(mutbur_bp)
+    df_pb_line = mk_df_pb(chr_mu_dict,30)
+    df2RT[counter+1]=df_pb_line["sum"].cumsum()
+    mutbur_pb=df_pb_line['sum'].sum()
+    pblist.append(mutbur_pb)
     counter += 1
 print("The average mutation burden for Poisson after 30 div is",mean(plist),"+/-",stdev(plist))
-print("The average mutation burden for binomial-Poisson after 30 div is",mean(bplist),"+/-",stdev(bplist))
+print("The average mutation burden for Poisson-binomial after 30 div is",mean(pblist),"+/-",stdev(pblist))
 print("Index of Dispersion for Poisson after 30 div, n=",n,",",(stdev(plist))**2/mean(plist))
-print("Index of Dispersion for binomial-Poisson after 30 div, n=",n,",",(stdev(bplist))**2/mean(bplist))
+print("Index of Dispersion for Poisson-binomial after 30 div, n=",n,",",(stdev(pblist))**2/mean(pblist))
 
 # Introduces a row of zeros into each dataframe to represent the starting point for each lineage.
 dfRT.loc[len(dfRT)]=0
@@ -138,25 +190,33 @@ df2RTm=pd.melt(df2RT,id_vars=['Div'],value_vars=list(df2RT.columns)[1:],var_name
 sns.set(style="white", palette="bright", color_codes=True, rc={"lines.linewidth": 1.0})
 plt.rcParams['patch.linewidth'] = 0
 
-# Plots Poisson vs binomial-Poisson distributions after 1 division.
-fig1, ax = plt.subplots(figsize=(2.5, 2.5))
+# Plots Poisson, Poisson-binomial, and gamma-Poisson-binomial distributions after 1 division.
+fig1, ax = plt.subplots(figsize=(2, 2.5))
 
 sns.distplot(df_p["sum"],
                   bins=list(range(900,3000,10)),
-                  kde=False,
+                hist=False,
+#                   kde=False,
                   norm_hist=True,
                   color='gray')
-sns.distplot(df_bp["sum"],
+sns.distplot(df_pb["sum"],
                   bins=list(range(900,3000,10)),
-                  kde=False,
+             hist=False,
+#                   kde=False,
                   norm_hist=True,
                   color='orange')
+sns.distplot(df2_nb["sum"],
+                  bins=list(range(900,3000,10)),
+             hist=False,
+#                   kde=False,
+                  norm_hist=True,
+                  color='green')
 ax.tick_params(bottom=True,left=True,labelsize=10)
 ax.set(xlabel='Mutations/Division', ylabel='Density')
 
-fig1.savefig(f"{File_save_location}/ASmut_hu.pdf",transparent = True,bbox_inches='tight')
+fig1.savefig(f"{File_save_location}/ASmut_hu530.pdf",transparent = True,bbox_inches='tight')
 
-# Plots Poisson vs binomial-Poisson distributions after 1 division.
+# Plots Poisson vs Poisson-binomial distributions after 30 divisions.
 fig2, ax = plt.subplots(figsize=(2.5, 2.5))
 
 sns.distplot(plist,
@@ -164,7 +224,7 @@ sns.distplot(plist,
                   kde=False,
                   norm_hist=True,
                   color='gray')
-sns.distplot(bplist,
+sns.distplot(pblist,
                   bins=list(range(51000,63000,200)),
                   kde=False,
                   norm_hist=True,
@@ -174,7 +234,7 @@ ax.set(xlabel='Mutation Burden', ylabel='Density')
 
 fig2.savefig(f"{File_save_location}/MB_hu30.pdf",transparent = True,bbox_inches='tight')
 
-# Plots Poisson vs binomial-Poisson mutation accumulation over 10 divisions.
+# Plots Poisson vs Poisson-binomial mutation accumulation in individual lines over 10 divisions.
 fig3, ax = plt.subplots(figsize=(2,2.5))
 ax.tick_params(bottom=True,left=True,labelsize=10)
 ax.set(xlim=(0, 10),ylim=(0,25000))
@@ -182,3 +242,12 @@ sns.lineplot(x="Div", y="Total Mut", hue='line', palette=sns.color_palette("Set1
 sns.lineplot(x="Div", y="Total Mut",legend=False, lw=1,color="k",data=dfRTm)
 
 fig3.savefig(f"{File_save_location}/MB_huMutAc.pdf",transparent = True,bbox_inches='tight')
+
+# Plots changes in the index of dispersion as a function of mutation rate.
+fig4, ax = plt.subplots(figsize=(2, 2.5))
+ax.tick_params(bottom=True,left=True,labelsize=10)
+ax.set(xlim=(0, 1000),ylim=(0,55))
+sns.regplot(x="rate", y="ID", data=Df_IDrng_p, color="gray",marker='o', scatter_kws={'s':1})
+sns.regplot(x="rate", y="ID", data=Df_IDrng_pb, color="orange",marker='o', scatter_kws={'s':1})
+
+fig4.savefig(f"{File_save_location}/MR_IDcorr.pdf",transparent = True,bbox_inches='tight')
